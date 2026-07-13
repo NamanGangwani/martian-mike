040ae38b2a2d6050b38a4f2961caa02882ed570e
NamanGangwani <NamanGangwani@outlook.com>
2026-07-12 21:19:44 -0500

    added player movement + camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,21 @@
 extends CharacterBody2D
 
+@export var gravity = 400
+@export var speed = 125
+@export var jump_force = 200
+
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("move_right"):
-		animated_sprite.play("run")
+func  _physics_process(delta: float) -> void:
+	if !is_on_floor():
+		velocity.y += gravity * delta
+		if velocity.y > 500:
+			velocity.y = 500
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force
+		
+	var direction : int = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	
+	move_and_slide()
