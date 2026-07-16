99aff183dbaba357fe46c002d313e1ec7de14356
NamanGangwani <NamanGangwani@outlook.com>
2026-07-15 22:53:50 -0500

    added start spawn

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
-@onready var start_position = $StartPosition
+@onready var start = $Start
 
-@onready var player = $Player
+var player = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+	if player != null:
+		player.global_position = start.get_spawn_pos()
 	var traps = get_tree().get_nodes_in_group("traps")
 	for trap in traps:
 		# trap.connect("touched_player", _on_trap_touched_player)
@@ -27,4 +30,4 @@ func _on_trap_touched_player() -> void:
 
 func reset_player() ->void:
 	player.velocity = Vector2.ZERO
-	player.global_position = start_position.global_position
+	player.global_position = start.get_spawn_pos()
---
+++ scripts/start.gd
@@ -0,0 +1,6 @@
+extends StaticBody2D
+
+@onready var spawn_position = $SpawnPosition
+
+func get_spawn_pos() -> Vector2:
+	return spawn_position.global_position
